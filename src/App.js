f91e0c7ebacf57df77febf46d4f06b6f2ae9dbdb
Jeremias Erbs <jerry@badabam.de>
2021-03-18 10:48:00 +0100

    Add /api/users

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 
 function App() {
   const [message, setMessage] = useState('')
+  const [users, setUsers] = useState([])
+
 
   useEffect(() => {
     fetch('/api').then(res => res.json()).then(data => setMessage(data.message))
+    fetch('/api/users').then(res => res.json()).then(setUsers)
   }, [])
   return (
     <div className="App">
-      {message}
+      <h1>{message}</h1>
+      <ul>
+        {users.map((user, index) => <li key={index}>{user.name}</li>)}
+      </ul>
     </div>
   );
 }
